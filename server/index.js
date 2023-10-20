199d3615d975aa0cb966fc42f08b41c58173074d
Abhishek Singh <abhinav210702@gmail.com>
2023-10-20 10:27:08 +0530

    minor

--- server/index.js
+++ server/index.js
@@ -21,16 +21,9 @@ const connectDB = async() => {
     }
 }
 
-const corsOptions = {
-    origin: 'https://craftzblog.vercel.app', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials:true
-  };
 
-app.use(cors(corsOptions))
+
+app.use(cors({origin:"https://craftzblog.vercel.app", credentials:true}))
 
 app.use((req, res, next) => {
     res.setHeader(
