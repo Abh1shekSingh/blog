171c6cf5728e359565e4594c12f079f4ba91010e
Abhishek Singh <abhinav210702@gmail.com>
2023-10-20 08:28:32 +0530

    some chanhes

--- server/index.js
+++ server/index.js
@@ -21,12 +21,31 @@ const connectDB = async() => {
     }
 }
 
+app.use(cors({origin:"https://craftzblog.netlify.app/",credentials:true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }))
 
-
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://craftzblog.netlify.app/"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+  });
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname,"/images")))
 app.use(cookieParser());
-app.use(cors({origin:"https://craftzblog.netlify.app/",credentials:true }))
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
 app.use('/api/posts', postRouter)
